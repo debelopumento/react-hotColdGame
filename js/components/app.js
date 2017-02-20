fb2b61119415fd6493dcfdc01ddf793d5b3941a7
debelopumento <imdiye@gmail.com>
2017-02-20 00:37:16 -0800

    reorganized containers

--- js/components/app.js
+++ js/components/app.js
@@ -1,26 +1,11 @@
 import React, {component} from 'react';
-import EnterGuess from '../containers/enterGuess';
-import HotOrCold from '../containers/hotOrCold';
-import GuessCount from '../containers/guessCount';
-import GuessHistory from '../containers/guessHistory';
-import NavBar from '../containers/navBar';
 import GameDoc from '../containers/gameDoc';
-import store from '../store';
+import GameContainer from '../containers/gameContainer';
 
 const App = () => {
 	return 	<div>
 				<GameDoc />
-				<div>
-					<nav>
-						<NavBar />
-					</nav>
-					<h2>HOT or COLD</h2>
-					<HotOrCold />
-					<EnterGuess />
-					<GuessCount />
-					<GuessHistory />
-				</div>
-				
+				<GameContainer />
 			</div>;
 }
 
---
+++ js/containers/gameContainer.js
@@ -0,0 +1,37 @@
+
+import React, {component} from 'react';
+import EnterGuess from './enterGuess';
+import HotOrCold from './hotOrCold';
+import GuessCount from './guessCount';
+import GuessHistory from './guessHistory';
+import NavBar from './navBar';
+import GameDoc from './gameDoc';
+import store from '../store';
+
+
+
+class GameContainer extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<nav>
+					<NavBar />
+				</nav>
+				<h2>HOT or COLD</h2>
+				<HotOrCold />
+				<EnterGuess />
+				<GuessCount />
+				<GuessHistory />
+			</div>
+		);
+	}
+}
+
+
+export default GameContainer;
